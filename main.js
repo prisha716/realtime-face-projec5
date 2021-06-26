5b2c8cfc6bfe5a36b0a42d46223cf569685d630a
prisha716 <84237696+prisha716@users.noreply.github.com>
2021-06-26 20:06:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
+
 function preload() {
 
 }
 function setup() {
 canvas=createCanvas(640,480);
 canvas.center();
-video=createCapture(VIDEO);
-video.hide();
+
 }
 function draw() {
-image(video,0,0,640,480)
+
 }
